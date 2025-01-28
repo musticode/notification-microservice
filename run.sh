6d7154f328450bc1b77291a788bedec701fd7a1f
mustafakaratas <mustafakaratas4179@gmail.com>
2025-01-28 17:57:42 +0300

    run.sh file updated

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
 echo "runner file"
+
+echo "Running docker compose file.."
+docker compose up -d
+
+echo "Starting config-server application"
+cd config-server
+mvn spring-boot:run
+
+echo "Starting service-discovery application"
+cd service-discovery
+mvn spring-boot:run
+
