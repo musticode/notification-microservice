057e2bd18d613e539935ca4cda87d63fc8b4bf9e
mustafakaratas <mustafakaratas4179@gmail.com>
2025-01-29 00:20:49 +0300

    auth filter added to gateway service update 2

--- api-gateway/src/main/java/com/notification/apigateway/jwt/JwtUtil.java
+++ api-gateway/src/main/java/com/notification/apigateway/jwt/JwtUtil.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Component;
 public class JwtUtil {
 
     public boolean isTokenValid(String token) {
+        //todo will be updated
         return false;
     }
 
     public String extractUsername(String token) {
+        //todo will be updated
         return "test";
     }
 
     public Object extractId(String token) {
+        //todo will be updated
         return null;
     }
 
